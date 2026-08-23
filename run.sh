b7e812c0891408f5cb1bc675cdce3a94aab4f3e4
Cary <cary@grokism.com>
2026-08-22 23:48:45 -0500

    Now boots, and writes a message.

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
-as -o multiboot_segment.o multiboot_segment.asm
-as -o boot.o boot.asm
+as -o multiboot_segment.o src/arch/x86_64/multiboot_segment.asm
+as -o boot.o src/arch/x86_64/boot.asm
 
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
-	ld -n -o kernel.bin -T linker.ld -e _start multiboot_segment.o boot.o
+	# --oformat binary
+	ld -n -o isofiles/boot/kernel.bin -T src/arch/x86_64/linker.ld -e _start multiboot_segment.o boot.o
 	
 	exit_code=$?
 	if [ $exit_code -eq 0 ]; then
 		rm boot.o
 		rm multiboot_segment.o
-		qemu-system-x86_64 kernel.bin
+		grub-mkrescue /usr/lib/grub/i386-pc -o linl.iso isofiles
+		qemu-system-x86_64 -drive file=linl.iso,format=raw,media=cdrom
 	fi
 fi
