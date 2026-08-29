e43d375cc883f88eaa7606fa94edcf0822313bc2
Cary <cary@grokism.com>
2026-08-29 11:23:49 -0500

    Added functionality for writing strings to VGA and added functionality for writing ints as formatted hex strings.

--- src/arch/x86_64/pre_long_util.asm
+++ src/arch/x86_64/pre_long_util.asm
@@ -1,101 +1,6 @@
 .code32
 .section .pre_long_mode_kernel
 
-# PrintCharVGA(x: int, y: int, c: char, color: u8) -> void
-.globl PrintCharVGA
-PrintCharVGA:
-	endbr32
-	push %ebx
-
-	cmp $0, %edi
-	jl PrintCharVGA_end			# if x < 0: jmp PrintCharVGA_end
-
-	cmp $79, %edi
-	jg PrintCharVGA_end			# if x > 99: jmp PrintCharVGA_end
-
-	cmp $0, %esi
-	jl PrintCharVGA_end			# if y < 0: jmp PrintCharVGA_end
-
-	cmp $24, %esi
-	jg PrintCharVGA_end			# if y > 39: jmp PrintCharVGA_end
-
-	shl $8, %cx
-	xor %dh, %dh
-	add %dx, %cx
-	mov %cx, %bx				# ebx = (ecx << 8) + edx
-
-	mov $80, %eax				
-	mul %esi				# eax = y * 80
-	add %edi, %eax				# eax = x + eax
-	mov $2, %esi
-	mul %esi				# eax = 2 * eax
-
-	add $0xb8000, %eax
-
-	movw %bx, (%eax)			# 0xb8000[eax] = low-half ebx
-	
-	PrintCharVGA_end:
-	pop %ebx
-	ret
-
-# PrintStrVGA(x: int, y: int, str: char*, color: u8) -> void
-.globl PrintStrVGA
-PrintStrVGA:
-	endbr32
-	push %ebx
-	push %ecx
-	
-	mov $0, %eax				# idx = 0
-
-	PrintStrVGA_loop:
-	push %eax
-	add %edx, %eax
-	movb (%eax), %bl			# ebx = edx[idx(= eax)]
-	pop %eax				# eax = idx
-
-	cmp $0, %bl
-	je PrintStrVGA_end_loop			# if edx[idx] == 0: jmp PrintStrVGA_end_loop
-
-	push %eax
-	push %edi
-	push %esi
-	push %edx
-	push %ecx
-	add %eax, %edi
-	mov %bl, %dl
-	mov (%esp), %ecx
-	call PrintCharVGA
-	pop %ecx
-	pop %edx
-	pop %esi
-	pop %edi
-	pop %eax
-
-	inc %eax
-	jmp PrintStrVGA_loop
-
-	PrintStrVGA_end_loop:
-	pop %ecx
-	pop %ebx
-	ret
-
-# ClearVGA(void) -> void
-.globl ClearVGA
-ClearVGA:
-	endbr32
-	
-	mov $0xb8000, %eax
-	
-	ClearVGA_loop:
-	cmp $0xb8fa0, %eax
-	jge ClearVGA_end_loop
-	movl $0x00000000, (%eax)
-	add $4, %eax
-	jmp ClearVGA_loop
-	ClearVGA_end_loop:
-
-	ret
-
 # WriteCharCOM1(c: char) -> void
 .globl WriteCharCOM1
 WriteCharCOM1:
@@ -121,7 +26,7 @@ StrLen:
 	movb (%ecx), %dl
 
 	cmp $0, %dl
-	jmp StrLen_end_loop
+	je StrLen_end_loop
 
 	inc %eax
 	jmp StrLen_loop
@@ -162,6 +67,89 @@ MemCpy:
 	push %eax
 	jmp MemCpy_loop
 	MemCpy_loop_end:
+
+	pop %eax
+	pop %edx
+	pop %esi
+	pop %edi
+	ret
+
+# MemCpyNonZero(arr1: int32*, arr2: int32*, len: int) -> void
+.globl MemCpyNonZero
+MemCpyNonZero:
+	endbr32
+
+	xor %eax, %eax
+	push %edi
+	push %esi
+	push %edx
+	push %eax
+	MemCpyNonZero_loop:
+	mov 4(%esp), %edx
+	mov (%esp), %eax
+	cmp %eax, %edx
+	jle MemCpyNonZero_loop_end
+
+	mov $4, %edx
+	mul %edx
+
+	mov 12(%esp), %edi
+	add %eax, %edi
+
+	mov 8(%esp), %esi
+	add %eax, %esi
+
+	movl (%edi), %eax
+	
+	cmp $0, %eax
+	je MemCpyNonZero_skip
+
+	movl %eax, (%esi)
+	MemCpyNonZero_skip:
+
+	pop %eax
+	inc %eax
+	push %eax
+	jmp MemCpyNonZero_loop
+	MemCpyNonZero_loop_end:
+
+	pop %eax
+	pop %edx
+	pop %esi
+	pop %edi
+	ret
+
+# MemFill(arr: int32*, val: int32, len: int32) -> void
+.globl MemFill
+MemFill:
+	endbr32
+
+	push %edi
+	push %esi
+	push %edx
+	xor %eax, %eax
+	push %eax
+	MemFill_loop:
+	mov (%esp), %eax
+	mov 4(%esp), %edx
+	cmp %eax, %edx
+	jle MemFill_end_loop
+
+	mov $4, %ecx
+	mul %ecx
+
+	mov 12(%esp), %edi
+	add %eax, %edi
+
+	mov 8(%esp), %esi
+
+	movl %esi, (%edi)
+
+	pop %eax
+	inc %eax
+	push %eax
+	jmp MemFill_loop
+	MemFill_end_loop:
 
 	pop %eax
 	pop %edx
--- src/arch/x86_64/pre_long_vga_out_stream.asm
+++ src/arch/x86_64/pre_long_vga_out_stream.asm
@@ -1,39 +1,68 @@
 .code32
 .section .pre_long_mode_kernel
 
-# WriteBuffer(void) -> void
-#.globl WriteBuffer
-#WriteBuffer:
-#	endbr32
-#	
-#	xor %eax, %eax					# idx = 0
-#	push %eax
-#
-#	WriteBuffer_loop:
-#	mov (%esp), %eax
-#	cmp $25, %eax
-#	jge WriteBuffer_end_loop
-#	
-#	mov (%esp), %edi
-#	call WriteBufferLine
-#
-#	mov (%esp), %eax
-#	inc %eax
-#	mov %eax, (%esp)
-#	jmp WriteBuffer_loop
-#
-#	WriteBuffer_end_loop:
-#	pop %eax
-#	ret
-
 # FlushBufferVGA(void) -> void
 .globl FlushBufferVGA
 FlushBufferVGA:
 	endbr32
 
+	mov $VGA_Out_Buffer.image_buffer, %edi
+	mov $0xb8000, %esi
+	mov $40*25, %edx
+	call MemCpy
+
 	mov $VGA_Out_Buffer.buffer, %edi
 	mov $0xb8000, %esi
 	mov $40*25, %edx
+	call MemCpyNonZero
+
+	ret
+
+# ClearBufferVGA(void) -> void
+.globl ClearBufferVGA
+ClearBufferVGA:
+	endbr32
+
+	mov $VGA_Out_Buffer.buffer, %edi
+	mov $0, %esi
+	mov $40*25, %edx
+	call MemFill
+
+	ret
+
+# ClearImageBufferVGA(void) -> void
+.globl ClearImageBufferVGA
+ClearImageBufferVGA:
+	endbr32
+
+	mov $VGA_Out_Buffer.image_buffer, %edi
+	mov $0, %esi
+	mov $40*25, %edx
+	call MemFill
+
+	ret
+
+# ClearBottomLineVGA(void) -> void
+.globl ClearBottomLineVGA
+ClearBottomLineVGA:
+	endbr32
+
+	mov $VGA_Out_Buffer.bottom_line, %edi
+	mov $0, %esi
+	mov $40, %edx
+	call MemFill
+
+	ret
+
+# LineShiftBufferVGA(void) -> void
+.globl LineShiftBufferVGA
+LineShiftBufferVGA:
+	endbr32
+
+	mov $80*2, %edi
+	add $VGA_Out_Buffer.buffer, %edi
+	mov $VGA_Out_Buffer.buffer, %esi
+	mov $40*24, %edx
 	call MemCpy
 
 	ret
@@ -43,10 +72,9 @@ FlushBufferVGA:
 WriteCharVGA:
 	endbr32
 
-	cmp %dl, '\n'
-	jne WriteCharVGA_skip_new_line
-	# if c == '\n' here...
-	WriteCharVGA_skip_new_line:
+	and $0x000000ff, %edx
+	cmp $'\n', %dl
+	je WriteCharVGA_reset_cursor
 
 	movb (VGA_Out_Buffer.current_color), %dh
 
@@ -57,18 +85,118 @@ WriteCharVGA:
 	add $VGA_Out_Buffer.bottom_line, %eax
 	movw %dx, (%eax)
 
-	cmp $78, %cl
-	jge WriteCharVGA_reset_cursor
+	cmp $158, %cl
+	jae WriteCharVGA_reset_cursor
 	add $2, %cl
 	movb %cl, (VGA_Out_Buffer.cursor_x)
 	jmp WriteCharVGA_end
 	WriteCharVGA_reset_cursor:
 	movb $0, (VGA_Out_Buffer.cursor_x)
-	#call LineShiftBufferVGA
+	call LineShiftBufferVGA
+
+	call ClearBottomLineVGA
 	WriteCharVGA_end:
 	ret
 
-# WriteStrVGA (str: char*) -> void
+# IntToSingleHex(i: u8) -> char
+.globl IntToSingleHex
+IntToSingleHex:
+	endbr32
+
+	cmp $10, %edi
+	jge IntToSingleHex_letters
+
+	add $'0', %edi
+	mov %edi, %eax
+
+	jmp IntToSingleHex_end
+	IntToSingleHex_letters:
+	
+	sub $10, %edi
+	add $'a', %edi
+	mov %edi, %eax
+
+	IntToSingleHex_end:
+
+	ret
+
+# WriteIntAsHexVGA(i: int32) -> char[12]
+.globl WriteIntAsHexVGA
+WriteIntAsHexVGA:
+	endbr32
+	sub $12, %esp
+
+	mov %esp, %esi
+
+	push %edi
+
+	movb $'0', (%esi)
+	movb $'x', 1(%esi)
+	
+	and $0xf0000000, %edi
+	shr $28, %edi
+	call IntToSingleHex
+
+	movb %al, 2(%esi)
+
+	mov (%esp), %edi
+	and $0x0f000000, %edi
+	shr $24, %edi
+	call IntToSingleHex
+
+	movb %al, 3(%esi)
+
+	mov (%esp), %edi
+	and $0x00f00000, %edi
+	shr $20, %edi
+	call IntToSingleHex
+
+	movb %al, 4(%esi)
+
+	mov (%esp), %edi
+	and $0x000f0000, %edi
+	shr $16, %edi
+	call IntToSingleHex
+
+	movb %al, 5(%esi)
+
+	mov (%esp), %edi
+	and $0x0000f000, %edi
+	shr $12, %edi
+	call IntToSingleHex
+
+	movb %al, 6(%esi)
+
+	mov (%esp), %edi
+	and $0x00000f00, %edi
+	shr $8, %edi
+	call IntToSingleHex
+
+	movb %al, 7(%esi)
+
+	mov (%esp), %edi
+	and $0x000000f0, %edi
+	shr $4, %edi
+	call IntToSingleHex
+
+	movb %al, 8(%esi)
+
+	mov (%esp), %edi
+	and $0x0000000f, %edi
+	call IntToSingleHex
+
+	movb %al, 9(%esi)
+
+	movw $0, 10(%esi)
+
+	mov %esi, %edi
+	call WriteStrVGA
+	
+	pop %edi
+	add $12, %esp
+	ret
+
+# WriteStrVGA(str: char*) -> void
 .globl WriteStrVGA
 WriteStrVGA:
 	endbr32
@@ -90,7 +218,10 @@ WriteStrVGA:
 	
 	xor %edx, %edx
 	movb (%edi), %dl
+	cmp $0, %dl
+	je WriteStrVGA_skip_write
 	call WriteCharVGA
+	WriteStrVGA_skip_write:
 
 	pop %eax
 	inc %eax
@@ -99,6 +230,122 @@ WriteStrVGA:
 	WriteStrVGA_end_loop:
 
 	add $12, %esp
+	ret
+
+# WriteImageCharBufferVGA(x: int, y: int, width: int, char_arr: char*, arr_len: int) -> void
+.globl WriteImageCharBufferVGA
+WriteImageCharBufferVGA:
+	endbr32
+	push %eax				# x
+	push %edi				# y
+	push %esi				# width
+	push %edx				# char_arr
+	push %ecx				# arr_len
+	push $0					# idx
+
+	mov $VGA_Out_Buffer.image_buffer, %edi
+	call WriteIntAsHexVGA
+
+	mov $' ', %edx
+	call WriteCharVGA
+
+	mov $'-', %edx
+	call WriteCharVGA
+
+	mov $' ', %edx
+	call WriteCharVGA
+
+	mov $VGA_Out_Buffer.current_color, %edi
+	call WriteIntAsHexVGA
+
+	mov $'\n', %edx
+	call WriteCharVGA
+
+	.equ x, 20
+	.equ y, 16
+	.equ width, 12
+	.equ char_arr, 8
+	.equ arr_len, 4
+	.equ idx, 0
+
+	WriteImageCharBufferVGA_loop:
+	mov idx(%esp), %eax
+	mov arr_len(%esp), %edx
+	cmp %eax, %edx
+	jle WriteImageCharBufferVGA_end_loop
+
+	mov char_arr(%esp), %edx
+	mov idx(%esp), %eax
+	add %edx, %eax
+	xor %ecx, %ecx
+	movb (%eax), %cl
+	push %ecx
+
+	mov idx+4(%esp), %eax
+	xor %edx, %edx
+	mov width+4(%esp), %ecx
+	div %ecx
+
+	mov x+4(%esp), %edi
+	add %edi, %edx				# edx = x in buffer
+	push %edx
+
+	mov y+4(%esp), %edi
+	add %edi, %eax				# eax = y in buffer
+	push %eax
+	
+	xor %edx, %edx
+	mov $160, %ecx
+	mul %ecx
+
+	mov $VGA_Out_Buffer.image_buffer, %edi
+	add %eax, %edi
+
+	pop %eax
+	pop %eax				# eax = x in buffer
+
+	xor %edx, %edx
+	mov $2, %ecx
+	mul %ecx
+
+	add %eax, %edi
+
+	push %edi
+	call WriteIntAsHexVGA
+
+	mov $' ', %edx
+	call WriteCharVGA
+	pop %edi
+
+	pop %eax
+
+	movb %al, 1(%edi)
+
+	pop %eax
+	inc %eax
+	push %eax
+	jmp WriteImageCharBufferVGA_loop
+	WriteImageCharBufferVGA_end_loop:
+
+	add $24, %esp
+	ret
+
+# WriteImageColorBufferVGA(x: int, y: int, width: int, color_arr: byte*, arr_len: int) -> void
+.globl WriteImageColorBufferVGA
+WriteImageColorBufferVGA:
+	endbr32
+	push %ebx				# To Preserve ebx
+	push %eax				# x
+	push %edi				# y
+	push %esi				# width
+	push %edx				# color_arr
+	push %ecx				# arr_len
+
+	.equ x, 16
+	mov $x, %eax
+
+	add $20, %esp
+	pop %ebx
 	ret
 
 # SetColor(color: u8) -> void
@@ -110,34 +357,10 @@ SetColor:
 
 	ret
 
-# WriteBufferLine(line: int) -> void
-#.globl WriteBufferLine
-#WriteBufferLine:
-#	endbr32
-#	push %edi
-#
-#	mov $2, %edx
-#	mov $80, %eax
-#	mul %edx
-#	
-#	mov (%esp), %edx
-#	mul %edx
-#	
-#	mov $VGA_Out_Buffer.buffer, %edi
-#	add %edx, %edi
-#
-#	mov $0xb8000, %esi
-#	add %edx, %esi
-#
-#	mov $40, %edx
-#	call MemCpy
-#
-#	pop %edi
-#	ret
-
 .section .data
 VGA_Out_Buffer:
 	VGA_Out_Buffer.buffer: .space 2 * 80 * 24
 	VGA_Out_Buffer.bottom_line: .space 2 * 80
+	VGA_Out_Buffer.image_buffer: .space 2 * 80 * 25
 	VGA_Out_Buffer.current_color: .space 1
 	VGA_Out_Buffer.cursor_x: .space 1
